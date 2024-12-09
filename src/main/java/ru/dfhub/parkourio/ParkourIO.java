a3da45f462d22ba37fb372f25e863d373d8be50e
Dertfin3051 <dertfin@gmail.com>
2024-12-09 21:52:57 +0300

    Configure CloudV2

--- src/main/java/ru/dfhub/parkourio/ParkourIO.java
+++ src/main/java/ru/dfhub/parkourio/ParkourIO.java
@@ -1,7 +1,10 @@
 package ru.dfhub.parkourio;
 
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.incendo.cloud.execution.ExecutionCoordinator;
+import org.incendo.cloud.paper.LegacyPaperCommandManager;
 import ru.dfhub.parkourio.components.Chat;
 import ru.dfhub.parkourio.components.JoinLeaveMessages;
 import ru.dfhub.parkourio.components.MOTD;
@@ -14,12 +17,17 @@ import static org.bukkit.Bukkit.getPluginManager;
 public final class ParkourIO extends JavaPlugin {
 
     private static ParkourIO instance;
+    private static LegacyPaperCommandManager<CommandSender> manager;
 
     @Override
     public void onEnable() {
         instance = this;
-        Config.reload();
+        manager = LegacyPaperCommandManager.createNative(
+                this,
+                ExecutionCoordinator.simpleCoordinator()
+        );
 
+        Config.reload();
         registerEvents(
                 new MOTD(),
                 new SpawnOnJoin(),
