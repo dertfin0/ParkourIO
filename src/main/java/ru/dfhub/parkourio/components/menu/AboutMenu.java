2eba1ba98604e239d528501ac738f4205b4bb774
Dertfin3051 <dertfin@gmail.com>
2024-12-27 20:38:05 +0300

    Add title to About menu

--- src/main/java/ru/dfhub/parkourio/components/menu/AboutMenu.java
+++ src/main/java/ru/dfhub/parkourio/components/menu/AboutMenu.java
@@ -5,13 +5,18 @@ import net.kyori.adventure.text.serializer.plain.PlainComponentSerializer;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.FixedMetadataValue;
 import org.json.JSONArray;
+import ru.dfhub.parkourio.ParkourIO;
 import ru.dfhub.parkourio.util.Config;
 import ru.dfhub.parkourio.util.IconItems;
 import ru.dfhub.parkourio.util.MenuFormatter;
+import ru.dfhub.parkourio.util.Metadata;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,7 +36,7 @@ public class AboutMenu {
             items.add(IconItems.serialize(itemsJson.getJSONObject(i)));
         }
 
-        inventory = MenuFormatter.getFormattedInventory(items);
+        inventory = MenuFormatter.getFormattedInventory(items, "О сервере");
     }
 
     public Inventory getInventory() {
@@ -47,6 +52,7 @@ public class AboutMenu {
             PlainTextComponentSerializer serializer = PlainTextComponentSerializer.builder().build();
             if (serializer.serialize(item.displayName()).equals("[О сервере]")) {
                 e.getPlayer().openInventory(new AboutMenu().getInventory());
+                e.getPlayer().setMetadata(Metadata.ABOUT_MENU.value(), new FixedMetadataValue(ParkourIO.getInstance(), true));
             }
         }
     }
