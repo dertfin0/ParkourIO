302af39ad328382f709c2ef8f4711256b48d0e6f
dertfin0 <dertfin@gmail.com>
2025-02-09 23:16:10 +0300

    Add spawn helper handler

--- src/main/java/ru/dfhub/parkourio/components/spawn/SpawnHandler.java
+++ src/main/java/ru/dfhub/parkourio/components/spawn/SpawnHandler.java
@@ -1,5 +1,6 @@
 package ru.dfhub.parkourio.components.spawn;
 
+import net.kyori.adventure.text.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
@@ -8,14 +9,23 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.json.JSONObject;
 import ru.dfhub.parkourio.ParkourIO;
+import ru.dfhub.parkourio.components.menu.ParkourMenu;
 import ru.dfhub.parkourio.components.parkour.ParkourItems;
 import ru.dfhub.parkourio.util.Config;
 import ru.dfhub.parkourio.util.Metadata;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import static net.kyori.adventure.text.minimessage.MiniMessage.miniMessage;
 
 public class SpawnHandler implements Listener {
 
@@ -53,6 +63,31 @@ public class SpawnHandler implements Listener {
         ) {
             e.getPlayer().setMetadata(Metadata.ON_PARKOUR_LEVEL.value(), new FixedMetadataValue(ParkourIO.getInstance(), Config.getConfig().getInt("spawn-parkour-level")));
             ParkourItems.give(e.getPlayer());
+        }
+    }
+
+    @EventHandler
+    public void onTalkWithHelper(PlayerInteractAtEntityEvent e) {
+        if (!e.getPlayer().getWorld().getName().equals(getSpawnLocation().getWorld().getName())) return;
+        if(e.getRightClicked().getLocation().getBlockX() != 20 || e.getRightClicked().getLocation().getBlockZ() != -4) return;
+        e.setCancelled(true);
+        CompletableFuture.runAsync(() -> {
+            try {
+                e.getPlayer().sendMessage(miniMessage().deserialize("<yellow>> Добро пожаловать на <gradient:#7733ff:#b82bff><b>ParkourIO</b></gradient>!</yellow>"));
+                Thread.sleep(1250);
+                e.getPlayer().sendMessage(miniMessage().deserialize("<yellow>> Здесь вы можете отточить свои навыки паркура, играя на разных уровнях - от простых, рассчитаных на конкретный навык, до длинных сложных уровней, которые собирают в себе самые сложные прыжки.</yellow>"));
+                Thread.sleep(4000);
+                e.getPlayer().sendMessage(miniMessage().deserialize("<yellow>> Пройти ознакомительный паркур можно, пройдя налево или нажав <click:run_command:'/parkour tp 0'><aqua>сюда</aqua></click>.</yellow>"));
+            } catch (InterruptedException ex) {}
+        });
+    }
+
+    @EventHandler
+    public void onNeedCloseDefaultInventory(InventoryOpenEvent e) {
+        if (List.of(
+                "Помощник"
+        ).contains(((TextComponent) e.getView().title()).content())) {
+            e.setCancelled(true);
         }
     }
 
